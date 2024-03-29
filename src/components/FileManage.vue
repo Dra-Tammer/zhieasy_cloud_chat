690b43ed9624ad5d63abaedbbb75d14588507cce
Dra-Tammer <2816677369@qq.com>
2024-03-29 17:54:06 +0800

    文件资源管理底部模块搭建

--- src/components/FileManage.vue
+++ src/components/FileManage.vue
@@ -1,223 +1,50 @@
 <template>
   <div class="knowledgeFileManagementContainer">
     <div class="fileManagementTop">
-      <vs-breadcrumb :items="[
-        {
-          title: 'knowledge',
-        },
-        {
-          title: 'Link 1',
-        },
-        {
-          title: 'Link 2',
-        },
-        {
-          title: 'Active',
-          active: true
-        }
-      ]"></vs-breadcrumb>
-      <div class="fileManagementTopButtonContainer">
-        <vs-button style="margin-right: 20px;" size="small" type="flat">新建文件夹</vs-button>
-        <vs-button size="small" type="flat">上传文件</vs-button>
-      </div>
+      <vs-button radius color="dark" type="flat" icon="chevron_left" style="margin-right: 20px;"></vs-button>
+      <div class="breadCrumbContainer">/knowledge/dir/item/zheshi/zhangsan</div>
     </div>
     <div class="fileListContainer">
-      <vs-table :data="users" maxHeight="100vh" search>
-        <template slot="thead">
-          <vs-th style="width: 20px;">
-          </vs-th>
-          <vs-th>
-            文件名
-          </vs-th>
-          <vs-th>
-            修改日期
-          </vs-th>
-          <vs-th>
-            类型
-          </vs-th>
-          <vs-th>
-            大小
-          </vs-th>
-          <vs-th>
-            操作
-          </vs-th>
-        </template>
 
-        <template slot-scope="{data}">
-          <vs-tr :key="indextr" v-for="(tr, indextr) in data">
-            <vs-td>
-              <img src="./file/dir.png" style="width: 22px;height: 22px;">
-            </vs-td>
-            <vs-td :data="data[indextr].email">
-              {{ data[indextr].email }}
-            </vs-td>
-
-            <vs-td :data="data[indextr].username">
-              {{ data[indextr].name }}
-            </vs-td>
-
-            <vs-td :data="data[indextr].id">
-              {{ data[indextr].id }}
-            </vs-td>
-            <vs-td>
-              <vs-td :data="data[indextr].website">
-                {{ data[indextr].website }}
-              </vs-td>
-            </vs-td>
-            <vs-td>
-              <vs-button size="small" type="flat" color="danger">删除</vs-button>
-
-            </vs-td>
-          </vs-tr>
-        </template>
-      </vs-table>
+    </div>
+    <div class="fileManageBottomContainer">
+      <vs-chip style="font-size: 16px;margin-left: 20px;margin-right: 20px;">
+        知识库： {{ knowledgeId }}
+      </vs-chip>
+      <vs-button size="large" color="primary" type="flat" icon="window" @click="newDirDialogActivePrompt = true;">
+        新建文件夹
+      </vs-button>
+      <vs-button size="large" color="success" type="flat" icon="upload" @click="uploadFileToKnowledge">
+        上传文件到知识库
+      </vs-button>
+      <vs-prompt
+          @cancel="newDirName=''"
+          @accept="newDir"
+          @close="newDirName=''"
+          :active.sync="newDirDialogActivePrompt"
+          title="新建文件夹"
+          accept-text="确定"
+          cancel-text="取消"
+      >
+        <div>
+          <vs-input placeholder="命名文件夹" v-model="newDirName" style="margin-top: 20px;"/>
+        </div>
+      </vs-prompt>
     </div>
   </div>
 </template>
 
 
 <script>
+
+
 export default {
   name: 'FileManage',
   data() {
     return {
-      knowledgeId: '',
-      breadCrumbItem: [
-        {
-          title: 'knowledge',
-        },
-        {
-          title: 'Link 1',
-        },
-        {
-          title: 'Link 2',
-        },
-        {
-          title: 'Active',
-          active: true
-        }
-      ],
-      users: [
-        {
-          "id": 1,
-          "name": "Leanne Graham",
-          "username": "Bret",
-          "email": "Sincere@april.biz",
-          "website": "hildegard.org",
-        },
-        {
-          "id": 2,
-          "name": "Ervin Howell",
-          "username": "Antonette",
-          "email": "Shanna@melissa.tv",
-          "website": "anastasia.net",
-        },
-        {
-          "id": 3,
-          "name": "Clementine Bauch",
-          "username": "Samantha",
-          "email": "Nathan@yesenia.net",
-          "website": "ramiro.info",
-        },
-        {
-          "id": 4,
-          "name": "Patricia Lebsack",
-          "username": "Karianne",
-          "email": "Julianne.OConner@kory.org",
-          "website": "kale.biz",
-        },
-        {
-          "id": 5,
-          "name": "Chelsey Dietrich",
-          "username": "Kamren",
-          "email": "Lucio_Hettinger@annie.ca",
-          "website": "demarco.info",
-        },
-        {
-          "id": 6,
-          "name": "Mrs. Dennis Schulist",
-          "username": "Leopoldo_Corkery",
-          "email": "Karley_Dach@jasper.info",
-          "website": "ola.org",
-        },
-        {
-          "id": 7,
-          "name": "Kurtis Weissnat",
-          "username": "Elwyn.Skiles",
-          "email": "Telly.Hoeger@billy.biz",
-          "website": "elvis.io",
-        },
-        {
-          "id": 8,
-          "name": "Nicholas Runolfsdottir V",
-          "username": "Maxime_Nienow",
-          "email": "Sherwood@rosamond.me",
-          "website": "jacynthe.com",
-        },
-        {
-          "id": 9,
-          "name": "Glenna Reichert",
-          "username": "Delphine",
-          "email": "Chaim_McDermott@dana.io",
-          "website": "conrad.com",
-        },
-        {
-          "id": 10,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 11,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 12,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 13,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 14,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 15,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 16,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        },
-        {
-          "id": 17,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton",
-          "email": "Rey.Padberg@karina.biz",
-          "website": "ambrose.net",
-        }
-      ]
+      knowledgeId: null,
+      newDirDialogActivePrompt: false,
+      newDirName: '',
     }
   },
   created() {
@@ -231,30 +58,47 @@ export default {
         }
     )
   },
-  methods: {},
+  methods: {
+    newDir() {
+      this.$vs.notify({
+        color: 'success',
+        title: '新建文件夹成功',
+        text: `成功新建：${this.newDirName}`
+      })
+      this.newDirName = ''
+    },
+    uploadFileToKnowledge() {
+
+    }
+  },
 }
 </script>
 
 <style lang="css" scoped>
 .knowledgeFileManagementContainer {
-  height: 100%;
+  height: 94%;
   width: 100%;
 }
 
 .fileManagementTop {
+  margin-top: 2px;
   height: 4%;
   border-bottom: 1px solid #c7c7c7;
   display: flex;
-  justify-content: space-between;
   align-items: center;
 }
 
-.fileManagementTopButtonContainer {
-  margin-right: 20px;
+.fileListContainer {
+  height: 86%;
 }
 
-.fileListContainer {
-  padding-top: 4px;
-  background-color: rgb(251, 251, 251);
+.fileManageBottomContainer {
+  display: flex;
+  position: fixed;
+  align-items: center;
+  bottom: 0;
+  height: 8%;
+  width: 41%;
+  border-top: 1px solid #c7c7c7;
 }
 </style>
