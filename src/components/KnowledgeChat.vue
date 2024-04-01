fd86e5e7f402f7008f7b8d8c4d953ccb3063b6f6
Dra-Tammer <2816677369@qq.com>
2024-04-01 23:28:47 +0800

    知识库组件优化

--- src/components/KnowledgeChat.vue
+++ src/components/KnowledgeChat.vue
@@ -17,142 +17,27 @@
           </div>
         </div>
         <div class="chatBoxResponse">
-          <div class="chatBoxResponseLeftContainer">
-            <vs-avatar text="AI" color="lightgreen"></vs-avatar>
-          </div>
-          <div class="chatBoxResponseRightContainer">
-            <div class="chatResponseFromKnowledge">
-              中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
+          <div class="streamResponse">
+            <div class="chatBoxResponseLeftContainer">
+              <vs-avatar text="AI" color="lightgreen"></vs-avatar>
+            </div>
+            <div class="chatBoxResponseRightContainer">
+              <div class="chatResponseFromKnowledge">
+                中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-
-      <div class="chatBox">
-        <div class="chatBoxTop">
-          <div class="chatBoxTopLeftContainer">
-            <vs-avatar/>
+          <div class="dividerBox">
+            <vs-divider position="left" color="#888">
+              <vs-icon icon="done_all" class="spanCite"></vs-icon>
+              <span style="color: #888888;">引用自</span>
+            </vs-divider>
           </div>
-          <div class="chatBoxTopRightContainer">
-            <div class="chatQuestions">
-              写出这份文件摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文这份文件的摘要件的摘要这份文件的摘要这份文件的摘要件的摘要
-            </div>
-            <div class="chatBottomDetailTime">2024-03-24 21:41:30</div>
+          <div class="citeContentBox">
+            <cite-file></cite-file>
           </div>
         </div>
-        <div class="chatBoxResponse">
-          <div class="chatBoxResponseLeftContainer">
-            <vs-avatar text="AI" color="lightgreen"></vs-avatar>
-          </div>
-          <div class="chatBoxResponseRightContainer">
-            <div class="chatResponseFromKnowledge">
-              中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
-            </div>
-          </div>
-        </div>
-      </div>
 
-
-      <div class="chatBox">
-        <div class="chatBoxTop">
-          <div class="chatBoxTopLeftContainer">
-            <vs-avatar/>
-          </div>
-          <div class="chatBoxTopRightContainer">
-            <div class="chatQuestions">
-              写出这份文件摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文这份文件的摘要件的摘要这份文件的摘要这份文件的摘要件的摘要
-            </div>
-            <div class="chatBottomDetailTime">2024-03-24 21:41:30</div>
-          </div>
-        </div>
-        <div class="chatBoxResponse">
-          <div class="chatBoxResponseLeftContainer">
-            <vs-avatar text="AI" color="lightgreen"></vs-avatar>
-          </div>
-          <div class="chatBoxResponseRightContainer">
-            <div class="chatResponseFromKnowledge">
-              中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-      <div class="chatBox">
-        <div class="chatBoxTop">
-          <div class="chatBoxTopLeftContainer">
-            <vs-avatar/>
-          </div>
-          <div class="chatBoxTopRightContainer">
-            <div class="chatQuestions">
-              写出这份文件摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文这份文件的摘要件的摘要这份文件的摘要这份文件的摘要件的摘要
-            </div>
-            <div class="chatBottomDetailTime">2024-03-24 21:41:30</div>
-          </div>
-        </div>
-        <div class="chatBoxResponse">
-          <div class="chatBoxResponseLeftContainer">
-            <vs-avatar text="AI" color="lightgreen"></vs-avatar>
-          </div>
-          <div class="chatBoxResponseRightContainer">
-            <div class="chatResponseFromKnowledge">
-              中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-      <div class="chatBox">
-        <div class="chatBoxTop">
-          <div class="chatBoxTopLeftContainer">
-            <vs-avatar/>
-          </div>
-          <div class="chatBoxTopRightContainer">
-            <div class="chatQuestions">
-              写出这份文件摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文这份文件的摘要件的摘要这份文件的摘要这份文件的摘要件的摘要
-            </div>
-            <div class="chatBottomDetailTime">2024-03-24 21:41:30</div>
-          </div>
-        </div>
-        <div class="chatBoxResponse">
-          <div class="chatBoxResponseLeftContainer">
-            <vs-avatar text="AI" color="lightgreen"></vs-avatar>
-          </div>
-          <div class="chatBoxResponseRightContainer">
-            <div class="chatResponseFromKnowledge">
-              中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="chatBox">
-        <div class="chatBoxTop">
-          <div class="chatBoxTopLeftContainer">
-            <vs-avatar/>
-          </div>
-          <div class="chatBoxTopRightContainer">
-            <div class="chatQuestions">
-              写出这份文件摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文件的摘要这份文这份文件的摘要件的摘要这份文件的摘要这份文件的摘要件的摘要
-            </div>
-            <div class="chatBottomDetailTime">2024-03-24 21:41:30</div>
-          </div>
-        </div>
-        <div class="chatBoxResponse">
-          <div class="chatBoxResponseLeftContainer">
-            <vs-avatar text="AI" color="lightgreen"></vs-avatar>
-          </div>
-          <div class="chatBoxResponseRightContainer">
-            <div class="chatResponseFromKnowledge">
-              中国大学生计算机设计大赛的参赛要求包括：参赛对象为全国高等院校在籍的本科生，也包括港、澳、台学生和留学生[1a]；指导教师应是在高校担任本科生教学任务的教师[2]；大赛作品须符合相关章程要求[1b]；作品内容分为软件应用与开发、微课与教学辅助、物联网应用、大数据应用、人工智能应用等类别[16]。
-            </div>
-          </div>
-        </div>
       </div>
 
 
@@ -169,12 +54,20 @@
 
 
 <script>
+import CiteFile from "@/components/citeFile.vue";
+
 export default {
   name: 'KnowledgeChat',
+  components: {CiteFile},
   data() {
     return {
       knowledgeId: '',
-      userInputMessage: ''
+      userInputMessage: '',
+      itemList: [
+        {id: 11, name: 'item 1'},
+        {id: 12, name: 'item 2'},
+        {id: 13, name: 'item 3'}
+      ]
     }
   },
   created() {
@@ -256,9 +149,13 @@ export default {
 }
 
 .chatBoxResponse {
+  height: 800px;
   background-color: #fafafa;
   box-shadow: 0 1px 0 #c7c7c7, 0 -1px 0 #c7c7c7;
   cursor: pointer;
+}
+
+.streamResponse {
   display: flex;
 }
 
@@ -278,7 +175,25 @@ export default {
 
 .chatResponseFromKnowledge {
   font-size: 14px;
-  margin-bottom: 20px;
+  margin-bottom: 6px;
+}
+
+.dividerBox {
+  width: 90%;
+  margin-left: 30px;
+  color: #888888;
+}
+
+.spanCite {
+  color: green;
+  font-size: 20px;
+}
+
+.citeContentBox {
+  width: 90%;
+  height: 200px;
+  background-color: #4CAF50;
+  margin-left: 30px;
 }
 
 .bottomInputBox {
